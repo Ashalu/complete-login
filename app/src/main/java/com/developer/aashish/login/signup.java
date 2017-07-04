7ccf44282bf1c17d9600f61ce1072977850a3ddd
Developer.Aashish <akbrother02@gmail.com>
2017-07-04 15:51:08 +0530

    hello

--- app/src/main/java/com/developer/aashish/login/signup.java
+++ app/src/main/java/com/developer/aashish/login/signup.java
@@ -2,6 +2,8 @@ package com.developer.aashish.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -20,61 +22,72 @@ public class signup extends AppCompatActivity {
 
     CheckBox ch;
     Button btn;
-EditText user,email,pass,repass;
-    RadioButton r1,r2;
-    String u,e,p,r;
+    EditText user, email;
+    TextInputEditText pass, repass;
+    RadioButton r1, r2;
+    String u, e, p, r;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-        Spinner my=(Spinner)findViewById(R.id.spn);
-        ArrayAdapter<String> mya=new ArrayAdapter<String>(signup.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.country));
+        Spinner my = (Spinner) findViewById(R.id.spn);
+        ArrayAdapter<String> mya = new ArrayAdapter<String>(signup.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.country));
         mya.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         my.setAdapter(mya);
-        btn=(Button)findViewById(R.id.button2);
-        ch=(CheckBox)findViewById(R.id.checkBox);
+        btn = (Button) findViewById(R.id.button2);
+        ch = (CheckBox) findViewById(R.id.checkBox);
+        email = (EditText) findViewById(R.id.email);
+        user = (EditText) findViewById(R.id.username);
+        pass = (TextInputEditText) findViewById(R.id.vpassword);
+        repass = (TextInputEditText) findViewById(R.id.vrepassword);
         ch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ch.isChecked())
-                {
-                 btn.setEnabled(true)  ;
-                }
-                else
-                {
+                if (ch.isChecked()) {
+                    btn.setEnabled(true);
+                } else {
                     btn.setEnabled(false);
                 }
             }
         });
 
-user=(EditText)findViewById(R.id.username);
-        u=user.getText().toString();
-        email=(EditText)findViewById(R.id.email);
-        e=email.getText().toString();
-        pass=(EditText)findViewById(R.id.password);
-        p=pass.getText().toString();
-        repass=(EditText)findViewById(R.id.repassword);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(user.equals("")||email.equals("")||pass.equals("")||repass.equals(""))
-                {
-                    Toast.makeText(signup.this,"Fill all block",Toast.LENGTH_LONG).show();
 
+                u = user.getText().toString();
+
+                e = email.getText().toString();
+
+                p = pass.getText().toString();
+
+                r = repass.getText().toString();
+
+                if (u.equals("") || e.equals("") || p.equals("") || r.equals("")) {
+                    Toast.makeText(signup.this, "Fill all block", Toast.LENGTH_LONG).show();
+
+                } else if (!p.equals(r)) {
+                    Toast.makeText(signup.this, "Repassword not matched", Toast.LENGTH_LONG).show();
                 }
-                else if(!pass.equals(repass))
+                else if (!e.matches(emailPattern))
                 {
-                    Toast.makeText(signup.this,"Repassword not matched",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"Invalid email address",Toast.LENGTH_SHORT).show();
                 }
-                else
-                {
-                    Intent i=new Intent(signup.this,MainActivity.class);
+
+
+
+
+
+
+                    else {
+                    Intent i = new Intent(signup.this, MainActivity.class);
                     startActivity(i);
                 }
             }
         });
     }
 }
-
 
