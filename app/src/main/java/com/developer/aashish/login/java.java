e47d220edf8cf2d32eea6bfab97ee937153e8d95
Developer.Aashish <akbrother02@gmail.com>
2017-07-06 10:21:50 +0530

    him

---
+++ app/src/main/java/com/developer/aashish/login/java.java
@@ -0,0 +1,25 @@
+package com.developer.aashish.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Aashish on 7/5/2017.
+ */
+
+public class java extends Fragment{
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.java,container,false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("java");
+    }
+}
